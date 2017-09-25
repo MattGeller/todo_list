d561ce2c9c49a45ca61ed66fb3643442dccead64
Matt Geller <MattG92@gmail.com>
2017-09-25 13:48:30 -0700

    todo list in progress

--- src/components/todo_list.js
+++ src/components/todo_list.js
@@ -1,13 +1,31 @@
 import React, { Component } from 'react';
+import {connect} from 'react-redux';
+import {getAll} from "../actions"; //same as '../actions/index'
+
 
 class TodoList extends Component {
+    componentWillMount(){
+        this.props.getAll();
+    }
+
     render(){
+        console.log('TodoList Props:', this.props);
+
+        const todosList = this.props.todos.map((item,index)=>{
+            return <li>{item.title}</li>
+        });
+
         return (
             <div>
-                <h1>Todo List</h1>
+                <h1 className="text-center mt-3">Todo List</h1>
             </div>
         )
     }
 }
+function mapStateToProps(state){
+    return {
+        todos: state.todos.all
+    }
+}
 
-export default TodoList;
+export default connect(mapStateToProps, {getAll})(TodoList);
